30862e4edad679383df1a018e3ae9283f0dee170
“iskandar” <iskandar.mee@gmail.com>
2025-01-31 16:18:47 +0700

    add config generate report and gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,25 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+      on('after:run', (results) => {
+        console.log("Event after:run berhasil dipanggil!", results);
+      });
+
+      return config;
+
     },
+
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      reportFilename: `report-${new Date().toISOString().replace(/:/g, '-')}`,
+      overwrite: false,
+      html: true,
+      json: true,
+    }
+
   },
 });
